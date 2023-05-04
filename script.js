c531bdd71427d7c60e73744f1ffefd87ee909b88
JosePire <121160943+JosePire@users.noreply.github.com>
2023-05-04 22:03:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const slides = [
+    {
+        "image": "slide1.jpg",
+        "tagLine": "<p>Impressions tous formats <span>en boutique et en ligne</span></p>"
+
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "<p>Tirages haute définition grand format<span>pour vos bureaux et events</span></p >"
+    },
+    {
+        " image ": " slide3.jpg ",
+        " tagLine ": "<p>Grand choix de couleurs<span>de CMJN aux pantones</span></p > "
+    },
+    {
+        " image ": " slide4.jpg ",
+        " tagLine ": "<p>Autocollants<span>avec découpe laser sur mesure</span></p > "
+    }
+]
+
