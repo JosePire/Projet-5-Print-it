c756e789eef96c60293101724e24173a1120cd54
Jose <piresjose_92@hotmail.fr>
2023-05-12 14:32:01 +0200

    IW-15 IW-16 Rajout code

--- script.js
+++ script.js
@@ -1,40 +1,112 @@
 const slides = [
     {
         "image": "slide1.jpg",
-        "tagLine": "<p>Impressions tous formats <span>en boutique et en ligne</span></p>"
+        "tagLine": "<p>Impressions tous formats</p><span>en boutique et en ligne</span>"
 
     },
     {
         "image": "slide2.jpg",
-        "tagLine": "<p>Tirages haute définition grand format<span>pour vos bureaux et events</span></p >"
+        "tagLine": "<p>Tirages haute définition grand format</p><span>pour vos bureaux et events</span>"
     },
     {
-        " image ": " slide3.jpg ",
-        " tagLine ": "<p>Grand choix de couleurs<span>de CMJN aux pantones</span></p > "
+        "image": "slide3.jpg",
+        "tagLine": "<p>Grand choix de couleurs</p><span>de CMJN aux pantones</span>"
     },
     {
-        " image ": " slide4.jpg ",
-        " tagLine ": "<p>Autocollants<span>avec découpe laser sur mesure</span></p > "
+        "image": "slide4.png",
+        "tagLine": "<p>Autocollants</p><span>avec découpe laser sur mesure</span>"
     }
 ]
 
-const btnPrev = document.getElementById('btn-prev')
-const btnNext = document.getElementById('btn-next')
+//slide en cours
+let currentSlide = 0;
 
-btnPrev.addEventListener('click', () => {
-    console.log('click btn précédent')
-})
+//on recup les flèches
+const arrowLeft = document.getElementById("arrow_left");
+const arrowRight = document.getElementById('arrow_right');
 
-btnNext.addEventListener('click', () => {
-    console.log('click btn suivant')
-})
-const dots = document.querySelector('.dots')
-const dot = document.createElement('span')
-dot.setAttribute('class', 'dot')
-dots.appendChild(dot)
+//nombre de slides
+const nbSlides = slides.length;
 
-let slide = [1, 1, 2, 3, 5, 8, 13];
-for (var i = 0; i < slides.length; i++) {
-    console.log(slides[i]);
+//on ajoute les bullet point
+const divDots = document.getElementById("dots");
+for (let i = 0; i < nbSlides; i++) {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if (i == 0) {
+        dot.classList.add('dot_selected');
+    }
+    divDots.appendChild(dot);
 }
+
+//on recup tous les dots
+const dots = document.getElementsByClassName('dot');
+//on recup le p et span du banner
+const bannerP = document.getElementById('banner p');
+const bannerSpan = document.getElementById("banner span");
+
+
+
+// Ajout des événements de clic fleche gauche
+arrowLeft.addEventListener("click", function () {
+
+    //on supprime la class du dot selectionné précédent
+    dots[currentSlide].classList.remove('dot_selected');
+
+    //on met a jour le slide en cours
+    if (currentSlide == 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide--;
+    }
+
+    //on ajoute la class du dot selectionné
+    dots[currentSlide].classList.add('dot_selected');
+
+    //on modifie la source de l'image
+    document.getElementById('banner-img').src = "assets/IMAGES/Diaporama/" + slides[currentSlide].image;
+
+
+    //on créer un div temporaire qui sera égale au tagLine du tableau selon le slide en cours
+    const divTemp = document.createElement('div');
+    divTemp.innerHTML = slides[currentSlide].tagLine;
+
+    //on peut maintenant associer le contenu du p et span du div temporaire aux notres
+    bannerP.innerHTML = divTemp.querySelector('p').textContent;
+    bannerSpan.innerHTML = divTemp.querySelector('span').textContent;
+
+});
+
+// Ajout des événements de clic fleche droite
+arrowRight.addEventListener("click", function () {
+
+    //on supprime la class du dot selectionné précédent
+    dots[currentSlide].classList.remove('dot_selected');
+
+    //on met a jour le slide en cours
+    if (currentSlide == slides.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+
+    //on ajoute la class du dot selectionné
+    dots[currentSlide].classList.add('dot_selected');
+
+    //on modifie la source de l'image
+    document.getElementById('banner-img').src = "assets/IMAGES/Diaporama/" + slides[currentSlide].image;
+
+
+
+    //on créer un div temporaire qui sera égale au tagLine du tableau selon le slide en cours
+    const divTemp = document.createElement('div');
+    divTemp.innerHTML = slides[currentSlide].tagLine;
+
+    //on peut maintenant associer le contenu du p et span du div temporaire aux notres
+    bannerP.innerHTML = divTemp.querySelector('p').textContent;
+    bannerSpan.innerHTML = divTemp.querySelector('span').textContent;
+});
+
+
+
 
